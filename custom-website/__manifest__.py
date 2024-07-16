4f085cb1638026df2e30f93c82bd77e831cdeae1
CmestraR <cristiandmestra2005@gmail.com>
2024-07-16 17:24:58 -0500

    Modulo para la personalización de la pagina web

---
+++ custom-website/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "QR Code Generator",
+    'version': "1.0.1",
+    'depends': ['base', 'sale'],
+    'author': "Fixup.com ",
+    'category': "Sales",
+    'description': """
+        Modulo para customizar el website, este modulo contiene los archivos JavaScript y CSS para 
+    """,
+    'data': [
+        'views/',
+    ],
+    'installable': True,
+    'auto_install': False,
+
+}
