7fdf32bf38a0e97ce387ebec1944d007c8c0c64a
CmestraR <cristiandmestra2005@gmail.com>
2024-07-17 16:26:48 -0500

    [feature]:Deifnición de controlador

--- custom_website/controllers/main.py
+++ custom_website/controllers/main.py
@@ -1,12 +1,12 @@
-from odoo import http
-from odoo.http import request
+#from odoo import http
+#from odoo.http import request
 
-class MyModuleController(http.Controller):
+#class MyModuleController(http.Controller):
 
-    @http.route('/my_module/get_city_data', type='json', auth='public', methods=['POST'], csrf=False)
-    def get_city_data(self, city_name):
-        if city_name:
-            city = request.env['res.city'].sudo().search([('name', 'ilike', city_name)], limit=1)
-            if city:
-                return {'zipcode': city.zipcode, 'state_id': city.state_id.id}
-        return {'error': 'City not found'}
+#    @http.route('/my_module/get_city_data', type='json', auth='public', methods=['POST'], csrf=False)
+#    def get_city_data(self, city_name):
+#        if city_name:
+#            city = request.env['res.city'].sudo().search([('name', 'ilike', city_name)], limit=1)
+#            if city:
+#                return {'zipcode': city.zipcode, 'state_id': city.state_id.id}
+#        return {'error': 'City not found'}
